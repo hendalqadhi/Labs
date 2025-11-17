b243059f6f1caaf9ddb61e1c687a7103246ab19d
hendalqadhi <hendalqadi515@gmail.com>
2025-11-17 09:50:02 +0300

    Lab1

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -1,9 +1,25 @@
 package arrays;
 
 
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+         int numbers[]={1,2,3,4,5};
+         int nums[]= new int[4];
+//methods for display
+         //Traditional loop
+       for (int i=0;i<numbers.length;i++)
+            System.out.println("Index"+"["+i+"]"+numbers[i]);
+//for each
+        for(int num :numbers)
+            System.out.println(num);
+        //to string
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(nums));
+
+
+
     }
 }
