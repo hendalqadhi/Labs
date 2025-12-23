1d8f0685aae87d7d053fe3f8c035601eb83b651e
hendalqadhi <hendalqadi515@gmail.com>
2025-12-23 22:39:28 +0300

    Lab7- SinglyLinkedList-part2

---
+++ src/SinglylLinkedLlist/Main.java
@@ -0,0 +1,15 @@
+package SinglylLinkedLlist;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list=new SinglyLinkedList<Integer>();
+        SinglyLinkedList<String> list2=new SinglyLinkedList<String>();
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addLast(40);
+        list.addFirst(30);
+        list.removeFirst();
+        list.removeLast();
+        list.display();
+    }
+}
