629f7141d322b42b794aed91f80fcc717f638811
hendalqadhi <hendalqadi515@gmail.com>
2025-12-23 22:22:41 +0300

    Lab5- Arrays-part2

--- src/arrays/Arrayss.java
+++ src/arrays/Arrayss.java
@@ -3,44 +3,126 @@ package arrays;
 import java.util.Scanner;
 
 public class Arrayss {
-//Scaner method
-    Scanner inputt=new Scanner(System.in);
+    //Scaner method
+    Scanner inputt = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
+    Scanner newAssign = new Scanner(System.in);
 
 
     //display with traditional for
-    public void Travesal1 (int []a){
-        for (int i=0;i<a.length;i++)
-            System.out.println("Index = "+"["+i+"]"+a[i]);
+    public void Travesal1(int[] a) {
+        for (int i = 0; i < a.length; i++)
+            System.out.println("Index  " + "[" + i + "] =" + a[i]);
+    }
+
+    public void Travesal_r(int[] a) {
+        for (int i = a.length - 1; i >= 0; i--)
+            System.out.println("Index  " + "[" + i + "] =" + a[i]);
     }
 
 
     //display with for each
-    public void Travesal2 (int []a) {
-        for(int e :a )
-        System.out.println(e);
+    public void Travesal2(int[] a) {
+        for (int e : a)
+            System.out.println(e);
 
         // scannering values from the user
     }
-        public void input (int []a){
-            System.out.println("Enter "+a.length+" numbers");
-            for (int i=0 ;i<a.length;i++)
-                a[i]= inputt.nextInt();
-        }
-        public void update(int []a, int index, int newValue){
-        if (index==0 || index>= a.length)
+
+    public void input(int[] a) {
+        System.out.println("Enter " + a.length + " numbers");
+        for (int i = 0; i < a.length; i++)
+            a[i] = inputt.nextInt();
+    }
+
+    //updating
+    public void update(int[] a, int index, int newValue) {
+        //System.out.println("Enter the index of the value you want to change");
+        //in.nextInt();
+        if (index == 0 || index >= a.length)
             System.out.println("The index is unvalid ");
         else
-        a[index] = newValue;
+            a[index] = newValue;
 
-        }
-    public void delete(int []a, int index){
-        if (index==0 || index>= a.length)
+    }
+
+    public void delete(int[] a, int index) {
+        if (index == 0 || index >= a.length)
             System.out.println("The index is unvalid ");
         else
             a[index] = 0;
 
     }
 
+    public void updating_user(int[] a) {
+        System.out.println("Enter the index of the value you want to update");
+        int index = in.nextInt();
+        System.out.println("Enter the new value ");
+        int newValue = newAssign.nextInt();
+        if (index == 0 || index >= a.length)
+            System.out.println("The index is unvalid ");
+        else
+            a[index] = newValue;
+
+
     }
+
+    public void shift_left_delete(int[] a, int index) {
+        for (int i = index; i < a.length - 1; i++)
+            a[i] = a[i + 1];
+
+
+        a[a.length - 1] = Integer.MIN_VALUE;
+    }
+
+    public int linear_serch(int[] a, int value) {
+        for (int i = 0; i < a.length; i++)
+            if (a[i] == value)
+                return i;
+        //-1 means not found
+        return -1;
+    }
+
+    public void shiftRightInsert(int[] a, int index, int value) {
+        if (index < 0 || index == a.length)
+            System.out.println("Enter correctly");
+        for (int i = a.length - 1; i > index; i--) {
+            a[i] = a[i - 1];
+        }
+
+
+        a[index] = value;
+
+    }
+
+    public int binarySearch(int[] a, int svalue, int findex, int lindex) {
+        while (findex <= lindex) {
+int mid =(findex+lindex)/2;
+if (svalue==a[mid])
+    return mid;
+if(svalue>a[mid])
+    findex=mid+1;
+if (svalue<a[mid])
+    lindex=mid-1;
+
+        }
+return -1;
+    }
+    public int binarySearch(int[] a, int svalue) {
+        int findex=0;
+        int lindex =a.length-1;
+        while (findex <= lindex) {
+            int mid =(findex+lindex)/2;
+            if (svalue==a[mid])
+                return mid;
+            if(svalue>a[mid])
+                findex=mid+1;
+            if (svalue<a[mid])
+                lindex=mid-1;
+
+        }
+        return -1;
+    }
+}
 
 
--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -7,17 +7,34 @@ public class Main {
     public static void main(String[] args) {
         // System.out.println("test");
          //array with values
-      //  int numbers[]={1,2,3,4,5};
+       int numbers[]={1,2,3,4,5};
 
-        //rray with default values (0)
+        //array with default values (0)
         int nums[]= new int[5];
+        //array of assignment
 
 //obj for using the methods
          Arrayss array=new Arrayss();
-         array.input(nums);
-         array.update(nums,0,0);
-         array.delete(nums,2);
-         array.Travesal1(nums);
+        // array.input(nums);
+         //array.update(nums,0,0);
+         //array.delete(nums,2);
+        // array.Travesal1(nums);
+         //array.updating_user(nums);
+        //array.Travesal1(nums);
+       // array.shift_left_delete(numbers,3);
+        //array.Travesal_r(numbers);
+
+        //int result= array.linear_serch(numbers,2);
+        int result=array.binarySearch(numbers,4,0,3);
+        if (result!=-1)
+           System.out.println("value found at index :"+result);
+        else
+             System.out.println(" value not found");
+        int number[]={2,3,4,5,6};
+
+        array.shiftRightInsert(number,0,1);
+        array.Travesal1(number);
+
 
         //to string
         //System.out.println(Arrays.toString(numbers));
