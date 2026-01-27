7bd67c42f20524df48c757544207983ccf723233
hendalqadhi <hendalqadi515@gmail.com>
2026-01-27 21:02:54 +0300

    QueueArray

--- src/Queue/main.java
+++ src/Queue/main.java
@@ -2,18 +2,22 @@ package Queue;
 
 public class main {
     public static void main(String[] args) {
-LinkedQueue<String> lQueue=new LinkedQueue<String>();
+/*LinkedQueue<String> lQueue=new LinkedQueue<String>();
 lQueue.enqueue("Hend");
 lQueue.enqueue("Sara");
 lQueue.enqueue("Somia");
 lQueue.enqueue("Asma");
 System.out.println("The first is:"+lQueue.front());
 while(!lQueue.isEmpty()){
-    System.out.println("element removed ="+lQueue.dequeue());
+    System.out.println("element removed ="+lQueue.dequeue());*/
+        ArrayQueue<String> aQueue=new ArrayQueue<String>();
+        aQueue.enqueue("Hend");
+        aQueue.enqueue("Henooda");
+        aQueue.enqueue("Hendo");
+        System.out.println("the front is :"+aQueue.front());
+        while (!aQueue.isEmpty()) {
+            System.out.println("element removed=" + aQueue.dequeue());
+        }
+}
 }
 
-
-
-
-    }
-}
